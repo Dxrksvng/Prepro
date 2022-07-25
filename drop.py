03138233c2ff47ba82605957fa39e7e69d6e9c79
Dxrksvng <109953373+Dxrksvng@users.noreply.github.com>
2022-07-25 16:29:23 +0700

    Add files via upload

---
+++ drop.py
@@ -0,0 +1,11 @@
+"""Drop"""
+def main():
+    """Drop"""
+    grade = float(input())
+    if grade < 1.00:
+        print("I'm so sad.")
+    elif grade < 2.00:
+        print("%.2f" %(4.00 - grade))
+    else:
+        print("I'm so happy.")
+main()
---
+++ ชู้กับเธอ.py
@@ -0,0 +1,11 @@
+"""ชู้กับเธอ"""
+def main():
+    """ชู้กับเธอ"""
+    money = int(input())
+    water = int(input())
+    snack1 =
+    snack2 =
+    snack3 =
+    answer = money - water
+    if answer 
+main()
